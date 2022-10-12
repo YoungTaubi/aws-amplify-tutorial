110b869bcc02d9f5df77637acbfbe55f94192471
YoungTaubi <jonasdarley@live.com>
2022-10-12 18:22:06 +0200

    picture upload

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -3,23 +3,41 @@ export type AmplifyDependentResourcesAttributes = {
         "awsamplifytutorial09335363": {
             "IdentityPoolId": "string",
             "IdentityPoolName": "string",
+            "HostedUIDomain": "string",
+            "OAuthMetadata": "string",
             "UserPoolId": "string",
             "UserPoolArn": "string",
             "UserPoolName": "string",
             "AppClientIDWeb": "string",
             "AppClientID": "string"
+        },
+        "userPoolGroups": {
+            "TestGroupRole": "string"
         }
     },
     "api": {
         "awsamplifytutorial": {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
+        },
+        "AdminQueries": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
         }
     },
     "storage": {
         "s30a838e94": {
             "BucketName": "string",
             "Region": "string"
+        }
+    },
+    "function": {
+        "AdminQueries2a666c2b": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
         }
     }
 }
--- src/pages/ProtectedPage.js
+++ src/pages/ProtectedPage.js
@@ -1,37 +1,103 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Storage from '@aws-amplify/storage';
+import { Auth, API } from 'aws-amplify';
 
 const ProtectedPage = () => {
 
+    const [adress, setAdress] = useState('')
+    const [profilePic, setProfilePic] = useState(null)
+    const [currentUser, setCurrentUser] = useState(null)
+
     Storage.configure({
         AWSS3: {
-            bucket: 'profile-pics174250-dev',
+            bucket: 'profile-pics152455-deloris',
             region: 'eu-central-1'
         }
     })
 
-    const S3ImageUpload = () => {
-        
+    const getCurrentUser = () => {
+        // Auth.currentUserCredentials()
+        // Auth.userData()
+        Auth.currentUserInfo()
+        .then(userData => {
+            setCurrentUser(userData)
+            console.log(userData);
+        })
+        .catch(err => {
+            console.log(err);
+        })
     }
-    
-    const onChange = async (file) => {
-            const { key } = await Storage.put('example.png', file, {
-                contentType: 'image/png'
-            })
 
-            console.log('S3 Object key', key)
+    const onChange = async (file) => {
+        const { key } = await Storage.put('example.png', file, {
+            contentType: 'image/png'
+        })
+
+        console.log('S3 Object key', key)
+    }
+
+    const handleSubmit = async (e) => { 
+        e.preventDefault()
+        const userPicName = 'userPic//'+currentUser.id     
+        const user = await Auth.currentAuthenticatedUser();
+        await Storage.put(userPicName, profilePic, {
+            // level: 'protected'
+        })
+        await Auth.updateUserAttributes(user, {
+            'address': adress,
+            'picture': userPicName
+        });
+        console.log(user);
+
+    }
+
+    let nextToken;
+
+    async function listEditors() {
+        let apiName = 'AdminQueries';
+        let path = '/listUsers';
+        let myInit = {
+            queryStringParameters: {
+                "groupname": "Editors",
+                "limit": 20,
+                "token": nextToken
+            },
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `${(await Auth.currentSession()).getAccessToken().getJwtToken()}`
+            }
         }
+        const { NextToken, ...rest } = await API.get(apiName, path, myInit);
+        nextToken = NextToken;
+        console.log(rest);
+        return rest;
+    }
+
+    listEditors()
+
+    useEffect(() => {
+        getCurrentUser()
+    }, [])
+
 
 
     return (
         <div>
             <h1>Protected Page</h1>
 
+            <form onSubmit={handleSubmit}>
+                <input type='text' onChange={e => setAdress(e.target.value)} placeholder='Adress' />
+                <input type='file' onChange={e => setProfilePic(e.target.file)} placeholder='Select Image' />
+                <button type='submit'>Save Changes</button>
+            </form>
+
+            <p>{currentUser?.attributes.picture}</p>
+            <p>{currentUser?.attributes.address}</p>
+
             <input
                 type='file'
-                accept='image/png'
                 onChange={(e) => onChange(e.target.files[0])}
-            />
+            /> 
         </div>
     );
 
