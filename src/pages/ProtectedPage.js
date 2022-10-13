e30a1f9f64489b7be0c41ef79d039f285073a639
YoungTaubi <jonasdarley@live.com>
2022-10-13 15:08:59 +0200

    test

--- src/pages/ProtectedPage.js
+++ src/pages/ProtectedPage.js
@@ -4,38 +4,38 @@ import { AuthContext } from "../context/auth"
 
 const ProtectedPage = () => {
 
-    // const { userContext, verifyUser } = useContext(AuthContext)
+    const { userContext, verifyUser } = useContext(AuthContext)
 
-    // const [name, setName] = useState(userContext?.attributes.name)
-    // const [address, setAddress] = useState(userContext?.attributes.address)
+    const [name, setName] = useState(userContext?.attributes.name)
+    const [address, setAddress] = useState(userContext?.attributes.address)
 
-    // const handleSubmit = async (e) => {
-    //     e.preventDefault()
-    //     await Auth.updateUserAttributes(userContext, {
-    //         'name': name,
-    //         'address': address
-    //     });
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        await Auth.updateUserAttributes(userContext, {
+            'name': name,
+            'address': address
+        });
 
-    //     verifyUser()
-    // }
+        verifyUser()
+    }
 
     return (
         <>
-            {/* <div>
+            <div>
                 <form onSubmit={handleSubmit}>
-                    <input type='text' onChange={e => {e.target.value.length > 0 && setName(e.target.value)}} placeholder='Name' />
-                    <input type='text' onChange={e => {e.target.value.length > 0 && setAddress(e.target.value)}} placeholder='Address' />
+                    <input type='text' onChange={e => { e.target.value.length > 0 && setName(e.target.value) }} placeholder='Name' />
+                    <input type='text' onChange={e => { e.target.value.length > 0 && setAddress(e.target.value) }} placeholder='Address' />
                     <button type='submit'>Save Changes</button>
                 </form>
-            </div> */}
+            </div>
             <div>
                 <h1>Protected Page</h1>
-                {/* <p>User E-mail: {userContext?.attributes.email}</p>
+                <p>User E-mail: {userContext?.attributes.email}</p>
                 <p>User Name: {userContext?.attributes.name}</p>
-                <p>User Address: {userContext?.attributes.address}</p> */}
+                <p>User Address: {userContext?.attributes.address}</p>
             </div>
         </>
     );
-};
+}
 
 export default ProtectedPage;
